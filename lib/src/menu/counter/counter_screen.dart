c7ecfd246508b6bfdb0a4450c3c477c777fa9c7c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-03 14:11:40 +0700

    :newspaper: add counter screen

---
+++ lib/src/menu/counter/counter_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text(
+          'Counter Screen',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _textCounter(),
+              _counterAction(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textCounter() {
+    return Text(
+      '7',
+      style: const TextStyle(
+        fontSize: 150,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _counterAction() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        MaterialButton(
+          color: Colors.teal,
+          onPressed: () {},
+          child: const Icon(
+            Icons.remove,
+            color: Colors.white,
+          ),
+        ),
+        MaterialButton(
+          color: Colors.teal,
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+}
