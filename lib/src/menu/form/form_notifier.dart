be8f53fa838d8d5ebdef525890b7e9ef5b09c7e7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-04 16:58:27 +0700

    :pencil: validate full name

---
+++ lib/src/menu/form/form_notifier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_playground/src/menu/form/form_state.dart';
+import 'package:riverpod_playground/src/utils/validation/full_name_validation.dart';
+
+final formNotifier =
+StateNotifierProvider.autoDispose<FormNotifier, FormState>((ref) {
+  return FormNotifier(const FormState());
+});
+
+class FormNotifier extends StateNotifier<FormState> {
+  FormNotifier(super._state);
+
+  void onFullNameChanged(String value) {
+    final fullName = FullName.dirty(value);
+    state = state.copyWith(
+      fullName: fullName,
+      fullNameError: fullName.error,
+    );
+  }
+
+}
--- lib/src/menu/form/widgets/input_full_name.dart
+++ lib/src/menu/form/widgets/input_full_name.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_playground/src/menu/form/form_notifier.dart';
 
-class InputFullName extends StatelessWidget {
+class InputFullName extends ConsumerWidget {
   const InputFullName({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final error = ref.watch(
+      formNotifier.select((state) => state.fullNameError),
+    );
     return TextField(
       decoration: InputDecoration(
         labelText: 'Full Name',
         hintText: 'your full name',
+        errorText: error,
       ),
       keyboardType: TextInputType.name,
       textInputAction: TextInputAction.next,
+      onChanged: (value) =>
+          ref.read(formNotifier.notifier).onFullNameChanged(value),
     );
   }
 }
