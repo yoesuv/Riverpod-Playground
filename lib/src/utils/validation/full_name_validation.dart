ba12b0c67417bd00fabd2beb618b0ae00a119f5a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-04 16:08:01 +0700

    :newspaper: create validation

---
+++ lib/src/utils/validation/full_name_validation.dart
@@ -0,0 +1,23 @@
+import 'package:formz/formz.dart';
+
+class FullName extends FormzInput<String, String> {
+
+  const FullName.pure(): super.pure('');
+  const FullName.dirty([String value = '']): super.dirty(value);
+
+  @override
+  String? validator(String value) {
+    // https://stackoverflow.com/a/57289735
+    RegExp regExp = RegExp(r'^[a-z A-Z,.\-]+$', caseSensitive: false);
+    if (value.isEmpty) {
+      return 'Full Name is Empty';
+    } else if (!regExp.hasMatch(value.trim())) {
+      return 'Full Name is Not Valid';
+    } else if (value.length >= 256) {
+      return 'Full Name is To Long';
+    } else {
+      return null;
+    }
+  }
+
+}
