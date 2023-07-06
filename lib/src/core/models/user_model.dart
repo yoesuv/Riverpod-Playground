58704ca929aff124465257892be96a8a582155e1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-06 12:01:15 +0700

    :newspaper: user model

---
+++ lib/src/core/models/user_model.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_playground/src/core/models/address_model.dart';
+import 'package:riverpod_playground/src/core/models/company_model.dart';
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    int? id,
+    String? name,
+    String? username,
+    String? email,
+    AddressModel? address,
+    String? phone,
+    String? website,
+    CompanyModel? company,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, Object?> json) =>
+      _$UserModelFromJson(json);
+}
