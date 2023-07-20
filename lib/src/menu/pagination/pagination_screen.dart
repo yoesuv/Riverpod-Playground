54fc69515614d66cede1e1bbcd70eec20dca24b0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-20 11:37:24 +0700

    :pencil: add scroll controller

--- lib/src/menu/pagination/pagination_screen.dart
+++ lib/src/menu/pagination/pagination_screen.dart
@@ -2,9 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_playground/src/menu/pagination/widgets/item_post.dart';
 import 'package:riverpod_playground/src/widgets/title_app_bar.dart';
 
-class PaginationScreen extends StatelessWidget {
+class PaginationScreen extends StatefulWidget {
   static const routeName = '/pagination';
+
   const PaginationScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PaginationScreenState();
+  }
+}
+
+class _PaginationScreenState extends State<PaginationScreen> {
+  final _controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      if (_controller.position.maxScrollExtent == _controller.offset) {
+        debugPrint('PaginationScreen # end off list');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +37,7 @@ class PaginationScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: ListView.separated(
+          controller: _controller,
           padding: const EdgeInsets.symmetric(vertical: 12),
           itemBuilder: (context, index) {
             return const ItemPost();
@@ -28,4 +49,9 @@ class PaginationScreen extends StatelessWidget {
     );
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 }
